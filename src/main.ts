495afdcca0ded7df09a183aa4ddde8b0e38d6683
itsaln <itsaln23@gmail.com>
2022-10-19 14:50:27 +0500

    init

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from '@app/app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+  await app.listen(5000)
+}
+
+bootstrap()
