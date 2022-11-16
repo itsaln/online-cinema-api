74e00fdd23ef1f6428918d249a0a2f9ad85918c5
itsaln <itsaln23@gmail.com>
2022-11-16 10:56:26 +0500

    deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from '@app/app.module'
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, { cors: true })
 	app.setGlobalPrefix('api')
-	await app.listen(5000)
+	await app.listen(process.env.port || 3000)
 }
 
 bootstrap()
