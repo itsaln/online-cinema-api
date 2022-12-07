9a1690a0715a602560972dd40cb7929a07430439
itsaln <itsaln23@gmail.com>
2022-12-07 14:33:05 +0500

    genre bug fix

--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
+import { Types } from 'mongoose'
 import { InjectModel } from 'nestjs-typegoose'
-import { ModelType } from '@typegoose/typegoose/lib/types'
+import { DocumentType, ModelType } from '@typegoose/typegoose/lib/types'
 import { GenreModel } from '@app/genre/genre.model'
 import { CreateGenreDto } from '@app/genre/dto/create-genre.dto'
 import { MovieService } from '@app/movie/movie.service'
@@ -13,23 +14,7 @@ export class GenreService {
 		private readonly movieService: MovieService
 	) {}
 
-	async findOne(_id: string) {
-		const genre = await this.GenreModel.findById(_id)
-
-		if (!genre) throw new NotFoundException('Genre not found')
-
-		return genre
-	}
-
-	async findBySlug(slug: string) {
-		const genre = await this.GenreModel.findOne({ slug }).exec()
-
-		if (!genre) throw new NotFoundException('Genre not found')
-
-		return genre
-	}
-
-	async findAll(searchTerm?: string) {
+	async findAll(searchTerm?: string): Promise<DocumentType<GenreModel>[]> {
 		let options = {}
 
 		if (searchTerm) {
@@ -48,15 +33,59 @@ export class GenreService {
 			}
 		}
 
-		return await this.GenreModel.find(options)
+		return await this.GenreModel
+			.find(options)
 			.select('-updatedAt -__v')
-			.sort({
-				createdAt: 'desc'
-			})
+			.sort({ createdAt: 'desc' })
 			.exec()
 	}
 
-	async create() {
+	async findBySlug(slug: string): Promise<DocumentType<GenreModel>> {
+		const genre = await this.GenreModel.findOne({ slug }).exec()
+
+		if (!genre) throw new NotFoundException('Genre not found')
+
+		return genre
+	}
+
+	async getPopular(): Promise<DocumentType<GenreModel>[]> {
+		return this.GenreModel
+			.find()
+			.select('-updatedAt -__v')
+			.sort({ createdAt: 'desc' })
+			.exec()
+	}
+
+	async getCollections(): Promise<ICollection[]> {
+		const genres = await this.findAll()
+
+		const collections = await Promise.all(
+			genres.map(async genre => {
+				const moviesByGenre = await this.movieService.findByGenres([genre._id])
+
+				const result: ICollection = {
+					_id: String(genre._id),
+					title: genre.name,
+					slug: genre.slug,
+					image: moviesByGenre[0]?.bigPoster
+				}
+
+				return result
+			})
+		)
+
+		return collections
+	}
+
+	async findOne(_id: string): Promise<DocumentType<GenreModel>> {
+		const genre = await this.GenreModel.findById(_id)
+
+		if (!genre) throw new NotFoundException('Genre not found')
+
+		return genre
+	}
+
+	async create(): Promise<Types.ObjectId> {
 		const defaultValue: CreateGenreDto = {
 			name: '',
 			slug: '',
@@ -67,7 +96,7 @@ export class GenreService {
 		return genre._id
 	}
 
-	async update(_id: string, dto: CreateGenreDto) {
+	async update(_id: string, dto: CreateGenreDto): Promise<DocumentType<GenreModel> | null> {
 		const updateGenre = await this.GenreModel.findByIdAndUpdate(_id, dto, {
 			new: true
 		}).exec()
@@ -77,32 +106,11 @@ export class GenreService {
 		return updateGenre
 	}
 
-	async delete(_id: string) {
+	async delete(_id: string): Promise<DocumentType<GenreModel> | null> {
 		const deleteGenre = await this.GenreModel.findByIdAndDelete(_id).exec()
 
 		if (!deleteGenre) throw new NotFoundException('Genre not found')
 
 		return deleteGenre
-	}
-
-	async getCollections() {
-		const genres = await this.findAll()
-		const collections = await Promise.all(
-			genres.map(async genre => {
-				const movieByGenre = await this.movieService.findByGenres([genre._id])
-				console.log(movieByGenre[0].bigPoster)
-
-				const result: ICollection = {
-					_id: String(genre._id),
-					image: movieByGenre[0].bigPoster,
-					slug: genre.slug,
-					title: genre.name
-				}
-
-				return result
-			})
-		)
-
-		return collections
 	}
 }
