2a86543a93bbe22d03b9299fcb8a09efda76fe44
itsaln <itsaln23@gmail.com>
2022-12-22 12:04:53 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 	// SwaggerModule.setup('api', app, document)
 
 	app.setGlobalPrefix('api')
-	await app.listen(process.env.port || 5000)
+	await app.listen(process.env.PORT || 5000)
 	console.log(`Application is running on: ${await app.getUrl()}`)
 }
 
