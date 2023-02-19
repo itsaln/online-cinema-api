fa73778284d7426dc0ba2dc3c1ac4511f26dc82d
itsaln <itsaln23@gmail.com>
2023-02-19 21:55:46 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from '@app/app.module'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create(AppModule, { cors: true })
+
+	// const options = new DocumentBuilder()
+	// 	.setTitle('Online Cinema API')
+	// 	.setDescription('Online cinema application for watching movies')
+	// 	.setVersion('1.0')
+	// 	.addBearerAuth()
+	// 	.build()
+
+	// const document = SwaggerModule.createDocument(app, options)
+	// SwaggerModule.setup('api', app, document)
 
 	app.setGlobalPrefix('api')
 	await app.listen(process.env.PORT || 5000)
