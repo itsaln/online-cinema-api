1f9972eaaf285a9d6d285dba208e7a5e4330009a
Aladin Mukhammadiev <itsaln23@gmail.com>
2024-12-10 18:47:20 +0500

    supergroup ID

--- src/config/telegram.config.ts
+++ src/config/telegram.config.ts
@@ -2,6 +2,6 @@ import { Telegram } from '@app/telegram/telegram.interface'
 
 export const getTelegramConfig = (): Telegram => ({
 	// https://api.telegram.org/bot212141231321:token/getUpdates
-	chatId: '-832701379',
+	chatId: '-1002068487280',
 	token: '5674967151:AAHhOBAN-JW5IZR0OQjAhREBGXI29w7cZuQ'
 })
